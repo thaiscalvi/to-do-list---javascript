40b0aeab309e159e9d493cb9427c4530866ed088
Thais Calvi <thaispinheiro@hotmail.com>
2025-06-11 10:54:18 -0300

    adicionando a opção de feito

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 function adicionarTarefa() {
   let inputTarefa = document.getElementById("inputTarefa");
-  let tarefa = inputTarefa.value.trim(); // remove espaços em branco
+  let tarefa = inputTarefa.value.trim();
   let mensagemElemento = document.getElementById("mensagem");
   let listaTarefas = document.getElementById("listaTarefas");
 
   if (tarefa === "") {
     mensagemElemento.textContent = "Não há tarefas. Favor adicionar uma tarefa.";
-    return; // interrompe a função
+    return;
   }
 
+  // Criar elementos
   let novaTarefa = document.createElement("li");
-  novaTarefa.textContent = tarefa;
+  novaTarefa.classList.add("tarefa-item");
 
+  let textoTarefa = document.createElement("span");
+  textoTarefa.classList.add("texto-tarefa");
+  textoTarefa.textContent = tarefa;
+
+  let bolinha = document.createElement("span");
+  bolinha.classList.add("status-bolinha");
+
+  // Alterna status ao clicar
+bolinha.addEventListener("click", () => {
+  bolinha.classList.toggle("finalizada");
+});
+
+  // Adicionar ao item
+  novaTarefa.appendChild(textoTarefa);
+  novaTarefa.appendChild(bolinha);
   listaTarefas.appendChild(novaTarefa);
+
   mensagemElemento.textContent = "Tarefa adicionada com sucesso!";
   inputTarefa.value = "";
 }
